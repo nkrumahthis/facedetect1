0e1ea06012b8f4925493d7425fb7f083e86cc088
nkrumahthis <inkusswamp@gmail.com>
2020-10-02 12:56:58 +0000

    detect faces

---
+++ app.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy
+
+# create a cascadeclassifier object
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("im1.jpeg")
+imgray = cv2.imread("im1.jpeg", 0)
+
+# g
+faces = face_cascade.detectMultiScale(imgray, scaleFactor = 1.05, minNeighbors = 5)
+
+for x,y,w,h in faces:
+    imgfaced = cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 3)
+
+print(type(faces))
+
+cv2.imshow("color", img)
+cv2.imshow("gray", imgray)
+cv2.imshow("faced", imgfaced)
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
